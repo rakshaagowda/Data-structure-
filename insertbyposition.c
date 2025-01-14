8e9e9da2a54839cfd5bbdc2ef5d326d210d7e794
RAKSHA B R <rakshagowdah264@gmail.com>
2025-01-14 18:16:54 +0530

    function to insert an element to an array based on position mentioned

---
+++ insertbyposition.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+#define MAX 5
+int n;void insertbypos(int* arr,int pos, int key){
+    n++;
+    if(n>MAX){
+        arr=(int *)realloc(arr,n*sizeof(int));
+        
+    }
+    if(pos<1||pos>n){
+        printf("invalid position");
+        return;
+    }
+    for(int i=n-1;i>=pos-1;i--){
+        *(arr+i)=*(arr+(i-1));
+    
+    }
+    *(arr+(pos-1))=key;
+}
