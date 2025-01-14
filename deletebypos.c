357a3e982e8d0cf577f3e37851f2515c9b787a41
RAKSHA B R <rakshagowdah264@gmail.com>
2025-01-14 18:18:57 +0530

    function to delete an element to an array based on position mentioned

---
+++ deletebypos.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+#define MAX 5
+int n;
+void deletebypos(int* arr,int pos){
+    if(pos<1||pos>n){
+        printf("invalid pos");
+        return;
+    }
+    printf("deleted %d\n",*(arr+(pos-1)));
+    n--;
+    for(int i=pos-1;i<n;i++){
+        *(arr+i)=*(arr+(i+1));
+    }
+}
