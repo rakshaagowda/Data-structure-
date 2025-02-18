5fd9a3fcd3216cf12f06b665acfd731efd2d9885
RAKSHA B R <rakshagowdah264@gmail.com>
2025-02-18 19:28:21 +0530

    add

---
+++ recursion/reversePrint.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+
+int print(int n){
+    
+    if(n==0)
+        return 0;
+    else{
+        printf("%d ",n);
+        return print(n-1);
+    }
+}
+int main()
+{
+	int n;
+    scanf("%d",&n);
+    printf("%d",print(n));
+	return 0;
+}
