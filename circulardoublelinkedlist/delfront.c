053f2053fdbdce80fa55ba30f7208abe25ad55a6
RAKSHA B R <rakshagowdah264@gmail.com>
2025-04-04 21:40:46 +0530

    add

---
+++ circulardoublelinkedlist/delfront.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+int count;
+struct node{
+    int data;
+    struct node *next,*prev;
+};
+typedef struct node *Node;
+int n;
+Node createnode(int data){
+    Node nn=(Node)malloc(sizeof(struct node));
+    if(nn==NULL){
+        printf("Memory allocation failed\n");
+       return nn; 
+    }
+    nn->next=nn;
+    nn->prev=nn;
+    nn->data=data;
+    
+    return nn;
+}
