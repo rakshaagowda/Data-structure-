49878cd2ef6cf4bf5bacee3053ecbb4a82e3b8a2
RAKSHA B R <rakshagowdah264@gmail.com>
2025-04-05 19:16:49 +0530

    added typedef and create node function

--- circulardoublelinkedlist/delrear.c
+++ circulardoublelinkedlist/delrear.c
@@ -5,3 +5,17 @@ struct node{
     int data;
     struct node *next,*prev;
 };
+typedef struct node *Node;
+int n;
+Node createnode(int data){
+    Node nn=(Node)malloc(sizeof(struct node));
+    if(nn==NULL){
+        printf("Memory allocation failed\n");
+       return nn; 
+    }
+    nn->next=nn;
+    nn->prev=nn;
+    nn->data=data;
+    
+    return nn;
+}
