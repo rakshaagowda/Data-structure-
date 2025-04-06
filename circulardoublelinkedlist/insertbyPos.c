7462dc63145966ae40c5b64695c4a39053d3e28a
RAKSHA B R <rakshagowdah264@gmail.com>
2025-04-06 17:34:01 +0530

    added insert  by position function

--- circulardoublelinkedlist/insertbyPos.c
+++ circulardoublelinkedlist/insertbyPos.c
@@ -19,3 +19,21 @@ Node createnode(int data){
   
   return nn;
 }
+void insertbypos(Node Header,int data,int pos){
+    Node nn=createnode(data);
+    if(pos<1||pos>count+1){
+        printf("invalid pos\n");
+        return;
+    }
+    Node temp=Header->next;
+    int i=1;
+    while(i<pos-1){
+        i++;
+        temp=temp->next;
+    }
+    nn->next=temp->next;
+    nn->prev=temp;
+    temp->next->prev=nn;
+    temp->next=nn;
+    count++;  
+}
