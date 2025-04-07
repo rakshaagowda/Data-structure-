afd910b3d107e6fc16134aa5734bb240edb13d7c
RAKSHA B R <rakshagowdah264@gmail.com>
2025-04-07 18:34:39 +0530

    addded create node function

--- circulardoublelinkedlist/insertbykey.c
+++ circulardoublelinkedlist/insertbykey.c
@@ -7,3 +7,15 @@ struct node{
 };
 typedef struct node *Node;
 int n;
+Node createnode(int data){
+  Node nn=(Node)malloc(sizeof(struct node));
+  if(nn==NULL){
+      printf("Memory allocation failed\n");
+     return nn; 
+  }
+  nn->next=nn;
+  nn->prev=nn;
+  nn->data=data;
+  
+  return nn;
+}
