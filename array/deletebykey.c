36b207986af26511384613b11960dd0865441a68
RAKSHA B R <rakshagowdah264@gmail.com>
2025-04-09 22:39:42 +0530

    added deletebykey function

--- array/deletebykey.c
+++ array/deletebykey.c
@@ -14,3 +14,26 @@ void display(int* ptr){
       printf("%d\t",*(ptr+i));
   }
 }
+void deletebykey(int* ptr,int key){
+  int flag=0;
+  for(int i=0;i<n;i++){
+      if(*(ptr+i)==key){
+          flag=1;
+          pos =i;
+          break;
+          }
+      
+      }
+      
+      if(flag == 0){
+          printf("\nkey not found");
+          return ;
+          }
+      else{
+          for(int i=pos;i<n-1;i++){
+              *(ptr+i)=*(ptr+(i+1));
+              
+          }
+          n--;
+      }
+  }
