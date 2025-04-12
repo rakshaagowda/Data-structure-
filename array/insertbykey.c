cc3d4be0c2d4463620bec841e09f14c65a77a016
RAKSHA B R <rakshagowdah264@gmail.com>
2025-04-12 22:52:22 +0530

    added

--- array/insertbykey.c
+++ array/insertbykey.c
@@ -2,3 +2,17 @@
 #include <stdlib.h>
 #define MAX 5
 int n;
+void read(int* ptr){
+  printf("enter %d elements:",n);
+  for(int i=0;i<n;i++){
+      scanf("%d", (ptr+i));
+      
+      }
+  }
+
+  
+void display(int* ptr){
+for(int i=0;i<n;i++){
+    printf("%d\t",*(ptr+i));
+}
+}
