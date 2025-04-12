4e6036fb7e8895887adb62dfed5f3cf763a9a93a
RAKSHA B R <rakshagowdah264@gmail.com>
2025-04-12 22:53:01 +0530

    added

--- array/insertbykey.c
+++ array/insertbykey.c
@@ -16,3 +16,10 @@ for(int i=0;i<n;i++){
     printf("%d\t",*(ptr+i));
 }
 }
+
+void insertbykey(int* ptr,int key,int pos){
+  ptr=(int*)realloc(ptr,(n+1)*sizeof(int));    
+  for(int i=n;i>pos;i--){
+      *(ptr+i)=*(ptr+(i-1));
+  }       
+  }
