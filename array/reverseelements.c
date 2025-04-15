04a4565354b51917157ef3d29af654818583c747
RAKSHA B R <rakshagowdah264@gmail.com>
2025-04-15 23:28:41 +0530

    added reverseelements function

--- array/reverseelements.c
+++ array/reverseelements.c
@@ -1,6 +1,14 @@
 
 #include <stdio.h>
 #include <stdlib.h>
+void reverse(int arr[], int n){
+  int temp;
+  for(int i=0; i<n/2; i++){
+    temp = arr[i];
+    arr[i] = arr[n-i-1];
+    arr[n-i-1] = temp;
+  }
+}
 int main(){
   int n;
   printf("Enter the number of elements: ");
