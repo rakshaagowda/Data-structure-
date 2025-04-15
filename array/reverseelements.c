dbdf961c3285ab142f59f46a0e71125073c8339d
RAKSHA B R <rakshagowdah264@gmail.com>
2025-04-15 23:27:28 +0530

    added

---
+++ array/reverseelements.c
@@ -0,0 +1,12 @@
+
+#include <stdio.h>
+#include <stdlib.h>
+int main(){
+  int n;
+  printf("Enter the number of elements: ");
+  scanf("%d", &n);
+  int arr[n];
+  printf("Enter the elements: ");
+  for(int i=0; i<n; i++){
+  
+}}
